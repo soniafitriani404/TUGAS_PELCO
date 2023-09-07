eaafaa9112779e09edf22c5880216e2b57bc3545
soniafitriani404 <143860584+soniafitriani404@users.noreply.github.com>
2023-09-07 07:45:43 +0700

    Add files via upload

---
+++ coding_ganjil.py
@@ -0,0 +1,5 @@
+celcius = int(input("masukkan suhu dalam bentuk celcius = "))
+# rumusnyaaa
+fahrenheit = (celcius*9/5) + 32
+#hasill
+print("suhu dalam fahrenheit", fahrenheit, "F")
---
+++ coding_ganjil_kelvin.py
@@ -0,0 +1,5 @@
+celcius = int(input("masukkan suhu dalam bentuk celcius = "))
+# rumusnyaaa
+kelvin = celcius + 273.25
+#hasill
+print("suhu dalam kelvin", kelvin, "K")
